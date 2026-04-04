07542812d20ada7c81ae3ae25816e2d7a94cd45b
Laura C <laurachaman@icloud.com>
2026-04-04 13:58:16 -0500

    Add Dependencies

--- src/main/java/com/example/enanospeleadores2/peleas/repository/PeleasRepository.java
+++ src/main/java/com/example/enanospeleadores2/peleas/repository/PeleasRepository.java
@@ -1,11 +1,11 @@
 package com.example.enanospeleadores2.peleas.repository;
 
-import com.example.enanospeleadores2.users.model.User;
 import com.example.enanospeleadores2.peleas.model.Pelea;
 import com.example.enanospeleadores2.peleador.model.Peleador;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.*;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface PeleasRepository extends JpaRepository<Pelea, Long> {
 
@@ -13,5 +13,5 @@ public interface PeleasRepository extends JpaRepository<Pelea, Long> {
 
     List<Pelea> findByGanador(Peleador ganador);
 
-    List<Pelea> findByUsuario(User usuario);
+    List<Pelea> findByFechaBetween(LocalDateTime inicio, LocalDateTime fin);
 }
